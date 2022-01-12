e05118470ce0538c4b7850632623a2796b8cfc48
ksetdekov <ksetdekov@gmail.com>
2022-01-12 17:17:17 +0300

    added new commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,6 +5,8 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустить бота"),
+            types.BotCommand("new", "Создать новую оптимизацию"),
+            types.BotCommand("select", "Выбрать оптимизацию"),
             types.BotCommand("help", "Вывести справку"),
         ]
     )
